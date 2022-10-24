1745b53155d31a8585365dbdf303fd3f8f444019
Deborah Obare <deborahobare14@gmail.com>
2022-10-24 11:18:07 +0100

    learning nodejs and installing node packages

---
+++ Backend/A_Nodejs_Fundamentals/app.js
@@ -0,0 +1,16 @@
+// create a variable
+var name1 = "Joseph"
+console.log(name1)
+// Be creative:
+// write a miniaturized javascript program excluding DOM here and
+// run it in the terminal
+
+var brands ="fila, nike, puma"
+console.log(brands)
+var array1 = [
+    ["apple, ball, car"],
+    ["dog, egg,fish"]
+]
+console.log(array1[1])
+
+
